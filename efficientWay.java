cba5bbb202ad27558b8ce755e9f03c1e7a07aa4b
Tanish Sawant <tanishsawant@rediffmail.com>
2020-07-20 14:19:47 +0530

    fantastic!!

---
+++ efficientWay.java
@@ -0,0 +1,41 @@
+public class efficientWay {
+    public static void main(String[] args) {
+        Runnable obj1 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Obj1");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Runnable obj2 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Obj2");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        t2.start();
+    } 
+}
