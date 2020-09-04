4709dcaf7fa3edc1733ac3450c3e829d60e9aef4
Tanish Sawant <tanishsawant@rediffmail.com>
2020-09-04 17:46:14 +0530

    infixtopostfix

--- infixToPostfix.java
+++ infixToPostfix.java
@@ -1,12 +1,46 @@
 public class infixToPostfix {
     public static void main(String[] args) {
-        StackArray<Character> operandStack = new <Character>StackArray();
-        StackArray<Character> operatorStack = new <Character>StackArray();
+        StackPointers<Character> operandStack = new <Character>StackPointers();
+        StackPointers<Character> operatorStack = new <Character>StackPointers();
         String expression = "a-(b+c*d)/e";
         for (char c : expression.toCharArray() ) {
-            if(Character.isLetterOrDigit(c)){
+            if(Character.isLetterOrDigit(c) && c!='(' && c!=')'){
+                operandStack.push(c);
+                System.out.println("Added " + c);
+            }
+            else if(Character.isWhitespace(c)){
                 
             }
+            else if(c == '+' || c == '-' || c == '*' || c == '/' || c == '('){
+                operatorStack.push(c);
+                System.out.println("Added " + c);
+            }
+            else if(c == ')'){
+                while (operatorStack.getTop() != '(') {
+                    char temp = operatorStack.getTop();
+                    operatorStack.pop();
+                    operandStack.push(temp);
+                    System.out.println("Added " + temp + " to operand");
+                }
+                operatorStack.pop();
+            }
         }
+            while (!operatorStack.isEmpty()) {
+                char temp = operatorStack.getTop();
+                operatorStack.pop();
+                operandStack.push(temp);
+            }
+        operatorStack = null;
+        //operandStack.push(expression.charAt(0));
+        StackPointers<Character> result = new <Character>StackPointers();
+        result.push(operandStack.getTop());
+        while(!operandStack.isEmpty()){
+            char temp = operandStack.getTop();
+            result.push(temp);
+            operandStack.pop();
+        } 
+        result.display();
+        operandStack.display();
+        operandStack = null;
     }    
 }
