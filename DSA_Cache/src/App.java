044a79d940ea8e84c5db6ca118c10e891163e44e
Tanish Sawant <tanishsawant@rediffmail.com>
2020-11-13 19:31:23 +0530

    added interface

--- DSA_Cache/src/App.java
+++ DSA_Cache/src/App.java
@@ -1,5 +1,38 @@
 public class App {
+    private interface Cache {
+        public int get(int key);
+        public void remove(int key);
+    }
+    
+    class LRUCache implements Cache{
+        dlinkedList dlist = new dlinkedList();
+        final int size = 5;
+
+        @Override
+        public int get(int key) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public void remove(int key) {
+            // TODO Auto-generated method stub
+
+        }
+    }
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
     }
 }
+
+
+// Doubly linked list
+// remove : O(1)
+// hashtable : O(n) => Quadratic probing.
+// Three Functions:
+// Get
+// remove
+// 
+
+// 
